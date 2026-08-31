e152d684dbedb510aff1cb62a52c7147e62a9017
pepesr-dev <180779764+pepesr-dev@users.noreply.github.com>
2026-08-31 14:36:45 +0200

    analisis en proceso

--- katas/typescript/factorialTail/factorialTail/src/factorialTail.ts
+++ katas/typescript/factorialTail/factorialTail/src/factorialTail.ts
@@ -1,8 +1,9 @@
 //todo - sacar primos limitantes en pc 
 // todo - y con otra base
 
-export const zeroes = (base: number, num: number) => {
-
+export const zeroes = (base: number, num: number)=> {
+  
+  
   return 25;
 };
 
